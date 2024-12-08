3dbf9497a36e1b3627ef1abbf11e401a440a201a
tanvir <tanvirulislam149@gmail.com>
2024-12-09 02:31:25 +0600

    4 done

--- assignment.py
+++ assignment.py
@@ -2,8 +2,8 @@ class Library:
     book_list = []
     def __init__(self, library_name):
         self.library_name = library_name
-    def entry_book(self, book_id, title, author, availability):
-        book = Book(book_id, title, author, availability)
+    def entry_book(self, book):
+        # book = Book(book_id, title, author, availability)
         self.book_list.append(book)
 
 
@@ -15,17 +15,33 @@ class Book:
         self.author = author
         self.availability = availability
 
-    def __repr__(self):
-        return 
+    @staticmethod
+    def borrow_book(book_id):
+        flag = False
+        for book in Library.book_list:
+            if book.book_id == book_id:
+                if book.availability == True:
+                    book.availability = False
+                    print(f"Here is your {book.title} book")
+                else:
+                    print("This book is already borrowed")
+                flag = True
+                break
+        if flag == False:
+            print("Book not found")
 
+ds = Book(3, "ds", "asdf", True)
+algo = Book(4, "algo", "qwer", True)
+db = Book(5, "db", "pciu", True)
 batighor = Library("batighor")
-batighor.entry_book(3, "Database book", "Jhankar Mahbub", True)
-batighor.entry_book(4, "DS", "Sumit Saha", True)
-batighor.entry_book(5, "Algorithm", "Rahat Khan", True)
+batighor.entry_book(ds)
+batighor.entry_book(algo)
+batighor.entry_book(db)
+for book in Library.book_list:
+    print(book.book_id, book.title, book.author, book.availability)
 
-pciu_lib = Library("pciu_lib")
-pciu_lib.entry_book(10, "SPL", "Morshed sir", True)
-pciu_lib.entry_book(10, "Physics", "Abadat sir", True)
+Book.borrow_book(4)
+Book.borrow_book(4)
 
-for book in batighor.book_list:
-    print(book.title)
+for book in Library.book_list:
+    print(book.book_id, book.title, book.author, book.availability)
