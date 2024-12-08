6a891ce3f85a8350d31628d8554096a4bf7aa4ab
tanvir <tanvirulislam149@gmail.com>
2024-12-09 00:43:19 +0600

    3 done

--- assignment.py
+++ assignment.py
@@ -1,7 +1,7 @@
 class Library:
     book_list = []
-    def __init__(self):
-        pass
+    def __init__(self, library_name):
+        self.library_name = library_name
     def entry_book(self, book_id, title, author, availability):
         book = Book(book_id, title, author, availability)
         self.book_list.append(book)
@@ -15,4 +15,17 @@ class Book:
         self.author = author
         self.availability = availability
 
-    
+    def __repr__(self):
+        return 
+
+batighor = Library("batighor")
+batighor.entry_book(3, "Database book", "Jhankar Mahbub", True)
+batighor.entry_book(4, "DS", "Sumit Saha", True)
+batighor.entry_book(5, "Algorithm", "Rahat Khan", True)
+
+pciu_lib = Library("pciu_lib")
+pciu_lib.entry_book(10, "SPL", "Morshed sir", True)
+pciu_lib.entry_book(10, "Physics", "Abadat sir", True)
+
+for book in batighor.book_list:
+    print(book.title)
